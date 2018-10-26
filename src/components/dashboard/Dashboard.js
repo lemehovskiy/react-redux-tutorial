cebfbce4893e631632bf01f7af194d509033fc6c
lemehovskiy <lemehovskiy@gmail.com>
2018-10-26 17:50:04 +0300

    lesson 27

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,41 +1,43 @@
 import React, {Component} from "react"
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
-import { connect } from 'react-redux'
-import { firestoreConnect } from 'react-redux-firebase'
-import { compose } from 'redux'
+import {connect} from 'react-redux'
+import {firestoreConnect} from 'react-redux-firebase'
+import {compose} from 'redux'
+import {Redirect} from 'react-router-dom'
 
+class Dashboard extends Component {
+    render() {
+        const {projects, auth} = this.props;
+        if (!auth.uid) return <Redirect to="/signin"/>
 
-class Dashboard extends Component{
-
-    render(){
-
-        const {projects} = this.props;
-
-        return(
-        <div className="dashboard container">
-            <div className="row">
-                <div className="col s12 m6">
-                    <ProjectList projects={projects}/>
-                </div>
-                <div className="col s12 m5 offset-m1">
-                    <Notifications/>
+        return (
+            <div className="dashboard container">
+                <div className="row">
+                    <div className="col s12 m6">
+                        <ProjectList projects={projects}/>
+                    </div>
+                    <div className="col s12 m5 offset-m1">
+                        <Notifications/>
+                    </div>
                 </div>
             </div>
-        </div>
         )
     }
 }
 
+
+
 const mapStateToProps = (state) => {
     return {
-        projects: state.firestore.ordered.projects
+        projects: state.firestore.ordered.projects,
+        auth: state.firebase.auth
     }
 }
 
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'projects'}
+        {collection: 'projects'}
     ])
 )(Dashboard)
